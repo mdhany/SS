8b178c97e5d2caf0e8f24f6c6972178afd3f5a63
Melvin <melvin@alsu.com.do>
2014-07-24 23:12:23 -0400

    Adding Front Page to SS

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,8 @@ class PagesController < ApplicationController
   def show
     @page = Admin::Page.find_by_url params[:url]
   end
+
+  def index
+    render layout: false
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -36,7 +36,7 @@ Rails.application.routes.draw do
     resources :pages, :logins, :payments, :turns, :levels, :capacities
   end
 
-  root 'logins#my_turns'
+  root 'pages#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
