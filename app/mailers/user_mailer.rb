f0050829d5a00abb22f703b2a4da0f181f122226
Melvin <melvin@alsu.com.do>
2014-07-29 01:15:57 -0400

    Testing y setting mail in production server

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,28 @@
+class UserMailer < ActionMailer::Base
+  default from: "no-reply@elsupersan.com"
+  layout 'email'
+
+  def welcome(user)
+    @user = user
+    @url  = 'http://elsupersan.com/sign_in'
+    mail(to: @user.email, subject: 'Bienvenido/a a elSuperSan.com')
+  end
+
+  def payment_sent_email(payment)
+    @user = payment.login
+    @url  = "http://elsupersan.com/payments/#{payment.id}/confirmation"
+    mail(to: @user.email, subject: 'Nuevo Pago Recibido - elSuperSan.com')
+  end
+
+  def payment_received_email(payment)
+    @user = payment.login
+    @payment = payment
+    @url  = "http://elsupersan.com/turn_wizard/#{payment.level_id}"
+    mail(to: @user.email, subject: 'Tu Pago ha sido Confirmado - elSuperSan.com')
+  end
+
+  def test
+    mail(to: 'melvin.dani7@gmail.com', subject: 'Tu Pago ha sido Confirmado - elSuperSan.com', body: "La prueba d eemail #{Time.now}")
+  end
+
+end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,12 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.sendmail_settings = {
+   location: '/usr/sbin/sendmail',
+   arguments: '-i -t'
+  }
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'elsupersan.com' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
